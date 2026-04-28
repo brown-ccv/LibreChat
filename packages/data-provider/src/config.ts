6663a6430ba820322c19bbe3e3b7fc827af1cadd
Camilo Diaz <k.diaz99@gmail.com>
2026-04-28 13:56:25 -0400

    maint: disabling delete user account button

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -671,6 +671,7 @@ export const interfaceSchema = z
         public: z.boolean().optional(),
       })
       .optional(),
+    deleteAccount: z.boolean().optional(),
   })
   .default({
     endpointsMenu: true,
@@ -718,6 +719,7 @@ export const interfaceSchema = z
       share: false,
       public: false,
     },
+    deleteAccount: true,
   });
 
 export type TInterfaceConfig = z.infer<typeof interfaceSchema>;
